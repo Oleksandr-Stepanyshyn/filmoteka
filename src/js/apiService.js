a8b359e8a0d4aa5e36aa0cd19c3096abc04f26bd
Oleksandr-Stepanyshyn <88288677+Oleksandr-Stepanyshyn@users.noreply.github.com>
2022-01-27 17:25:16 +0200

    added files

---
+++ src/js/apiService.js
@@ -0,0 +1,8 @@
+const API_KEY = 'c01f14dcdb58e9cec669b1017a4d540c';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+// const searchParams = new URLSearchParams({
+//     image_type: "photo",
+//     orientation: "horizontal",
+//     safesearch: true,
+//     per_page: 40, 
+// })
