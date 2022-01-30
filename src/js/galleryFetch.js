9f0f7739e6451875c9549e247c0af81dae5a62e0
Svetlana Trotsenko <lanalemishko@gmail.com>
2022-01-30 21:05:11 +0200

    add-render-errors

--- src/js/galleryFetch.js
+++ src/js/galleryFetch.js
@@ -1,5 +1,8 @@
 import cardMarkup from '../templates/cardMarkup';
+import errorsOnSearch from '../templates/errorsOnSearch';
+import errorOnEmptySearch from '../templates/errorOnEmptySearch';
 import FilmsApiService from './apiService';
+
 
 const refs = {
     formEl: document.querySelector(".form"),
@@ -27,18 +30,21 @@ renderDaylyTopFilms();
 function onFormElSubmit(e) { 
     e.preventDefault();
 
-    newFilmsBandle.query = e.target.elements.searchQuery.value.trim();
+    const name = e.target.elements.searchQuery.value.trim();
+
+    newFilmsBandle.query = name;
 
     if (!newFilmsBandle.query) { 
-        return onFilmsSearchError();
+        return onEmptySearchError();
     }
 
     galleryReset();
     
-    newFilmsBandle.onFetchKeyWordFilms()
+    newFilmsBandle.onFetchKeyWordFilms(name)
         .then((films) => {
             if (films.length === 0) {
-                return onFilmsSearchError();
+                console.log(name)
+                return onFilmsSearchError(name);
             }
             newFilmsBandle.incrementPageNumber();
             renderMarkup(films)
@@ -47,20 +53,52 @@ function onFormElSubmit(e) {
 }
 
 //рендер разметки галлереи фильмов
-function renderMarkup(films){
- refs.galleryEl.insertAdjacentHTML('beforeend', cardMarkup(films));
+function renderMarkup(films) {
+    const markup = films.map(
+        ({ poster_path, title, genre_ids, release_date, vote_average }) => {
+            const date = new Date(Date.parse(release_date));
+            const year = date.getFullYear();
+            const vote = Number(vote_average).toFixed(1);
+            let posterTmpl = '';
+            if (!poster_path) {
+                posterTmpl = "../images/template-poster.jpg";
+            }
+
+            let filmsInfo = {
+                poster_path,
+                title,
+                genre_ids,
+                year,
+                vote,
+                posterTmpl
+            }
+            return filmsInfo;
+        }
+    );
+
+ refs.galleryEl.insertAdjacentHTML('beforeend', cardMarkup(markup));
 }
 
 // перезагрузка галлереи
 function galleryReset() {
     newFilmsBandle.resetPageNumber();
     refs.galleryEl.innerHTML = '';
+    refs.errorEl.innerHTML = '';
     refs.errorEl.classList.add('visually-hidden');
 }
 
-// функция-ошибка, если поисковый запрос пустой, или фильма с таким названием не найдено
-function onFilmsSearchError() {
-    refs.errorEl.classList.remove('visually-hidden')
+// функция-ошибка, если фильма с таким названием не найдено
+function onFilmsSearchError(name) {
+    galleryReset()
+    refs.errorEl.insertAdjacentHTML('beforeend', errorsOnSearch({ name: name }));
+    refs.errorEl.classList.remove('visually-hidden');
+}
+
+// функция-ошибка, если поисковый запрос пустой
+function onEmptySearchError() {
+    galleryReset()
+    refs.errorEl.classList.remove('visually-hidden');
+    refs.errorEl.insertAdjacentHTML('beforeend', errorOnEmptySearch());
 }
 
 export default {};
