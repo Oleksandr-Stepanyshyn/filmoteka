b6156858276cca082b23c4898901ddc925124cd9
Svetlana Trotsenko <lanalemishko@gmail.com>
2022-01-31 00:05:22 +0200

    fix-renderOfErrors

--- src/js/galleryFetch.js
+++ src/js/galleryFetch.js
@@ -1,6 +1,4 @@
 import cardMarkup from '../templates/cardMarkup';
-import errorsOnSearch from '../templates/errorsOnSearch';
-import errorOnEmptySearch from '../templates/errorOnEmptySearch';
 import FilmsApiService from './apiService';
 
 
@@ -40,10 +38,9 @@ function onFormElSubmit(e) {
 
     galleryReset();
     
-    newFilmsBandle.onFetchKeyWordFilms(name)
+    newFilmsBandle.onFetchKeyWordFilms()
         .then((films) => {
             if (films.length === 0) {
-                console.log(name)
                 return onFilmsSearchError(name);
             }
             newFilmsBandle.incrementPageNumber();
@@ -55,23 +52,34 @@ function onFormElSubmit(e) {
 //рендер разметки галлереи фильмов
 function renderMarkup(films) {
     const markup = films.map(
-        ({ poster_path, title, genre_ids, release_date, vote_average }) => {
+        ({ poster_path, original_title, genre_ids, release_date, vote_average, original_name }) => {
             const date = new Date(Date.parse(release_date));
             const year = date.getFullYear();
             const vote = Number(vote_average).toFixed(1);
-            let posterTmpl = '';
-            if (!poster_path) {
-                posterTmpl = "../images/template-poster.jpg";
+            let filmName = original_title;
+            let genres = [];
+            if (genre_ids.length > 2) {
+                genres = `${genre_ids[0]}, ${genre_ids[1]}, other`
+            } else if(genre_ids.length === 0){
+                genres = 'other';
+            } else {
+                genres = genre_ids.join(', ');
+            }
+
+            if (!original_title) {
+                filmName = original_name;
             }
 
             let filmsInfo = {
                 poster_path,
-                title,
+                filmName,
                 genre_ids,
                 year,
                 vote,
-                posterTmpl
+                genres,
             }
+
+            console.log(filmsInfo);
             return filmsInfo;
         }
     );
@@ -90,15 +98,17 @@ function galleryReset() {
 // функция-ошибка, если фильма с таким названием не найдено
 function onFilmsSearchError(name) {
     galleryReset()
-    refs.errorEl.insertAdjacentHTML('beforeend', errorsOnSearch({ name: name }));
     refs.errorEl.classList.remove('visually-hidden');
+    const error = `<p>Search result <span class="film-name">"${name}"</span> not successful. Enter the correct movie name</p>`
+    refs.errorEl.insertAdjacentHTML('beforeend', error);
 }
 
 // функция-ошибка, если поисковый запрос пустой
 function onEmptySearchError() {
     galleryReset()
     refs.errorEl.classList.remove('visually-hidden');
-    refs.errorEl.insertAdjacentHTML('beforeend', errorOnEmptySearch());
+    const error = `<p>Field of search is empty, enter please keyword or words for begin search</p>`
+    refs.errorEl.insertAdjacentHTML('beforeend', error);
 }
 
 export default {};
