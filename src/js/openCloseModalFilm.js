7cf764c9aaadaaa27df6833ff43e6d7fc8b670c1
yulia-bisyuk <yulia.bisyuk@gmail.com>
2022-02-01 16:11:11 +0200

    changes accepted

--- src/js/openCloseModalFilm.js
+++ src/js/openCloseModalFilm.js
@@ -1,19 +1,11 @@
 const refs = {
   openModal: document.querySelector('.gallery__container'),
-<<<<<<< HEAD
-  // backdrop: document.querySelector('.backdrop')
-}
-refs.openModal.addEventListener("click", onOpenModal)
-// refs.backdrop.addEventListener("click", onBackdrop);
-
-=======
   backdrop: document.querySelector('.backdrop'),
   closeBtn: document.querySelector('[data-modal-close]')
 }
 refs.openModal.addEventListener("click", onOpenModal)
 refs.backdrop.addEventListener("click", onBackdrop);
 refs.closeBtn.addEventListener("click", onCloseModal)
->>>>>>> ce5e046a1559bee09c6ba07bcedf85e592cee400
 
 function onOpenModal() {
   window.addEventListener('keydown', onEscPress);
