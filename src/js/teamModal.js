5c025f1ccef5c51023dde4b080b089fce312c50e
Oleksandr-Stepanyshyn <88288677+Oleksandr-Stepanyshyn@users.noreply.github.com>
2022-02-05 19:40:36 +0200

    add files

--- src/js/teamModal.js
+++ src/js/teamModal.js
@@ -6,25 +6,26 @@ refs.btnCloseModalTeam.addEventListener('click', closeModalTeam);
 function openModalTeam() {
     console.log('hello');
     refs.modalTeam.classList.remove('is-hidden')
+    refs.bodyEl.classList.add('modal-open');
 
     document.addEventListener('keydown', onKeyboardPush);
     document.addEventListener('click', cliskIntoVoid);
 
     function onKeyboardPush(e) {
-    if (e.code === "Escape") {
-        refs.modalTeam.classList.add('is-hidden');
-        document.removeEventListener('keydown', onKeyboardPush);
-    }
+        if (e.code === "Escape") {
+            closeModalTeam(this);
+        }
     };
 
     function cliskIntoVoid(e) {
         if (e.target === refs.modalTeam) {
-            refs.modalTeam.classList.add('is-hidden');
-            document.removeEventListener('keydown', cliskIntoVoid);
+            closeModalTeam(this);
         }
     }
 }
 
-function closeModalTeam(e) {
+function closeModalTeam(fn) {
     refs.modalTeam.classList.add('is-hidden');
+    refs.bodyEl.classList.remove('modal-open');
+    document.removeEventListener('keydown', fn);
 }
