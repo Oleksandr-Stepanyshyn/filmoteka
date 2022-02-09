28f9916bbe4a0ceabf636190f7a1b9e8a3afcfc6
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-02-09 21:06:58 +0200

    Update trailer.js

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -7,7 +7,6 @@ export async function trailer(e) {
   filmTrailers
     .onfetchTrailers(Number(e.target.dataset.id))
     .then(data => {
-      console.log(data.results[0].key);
       trailerRender(data);
     })
     .catch(console.log);
@@ -26,6 +25,12 @@ function trailerRender(data) {
       onShow: instance => {
         instance.element().onclick = instance.close;
         document.addEventListener('keydown', onEscClose);
+      },
+    },
+    {
+      onClose: instance => {
+        document.removeEventListener('keydown', onEscClose);
+        console.log(instance);
       },
     },
   );
